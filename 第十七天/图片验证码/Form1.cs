e6b0f7aa04e85eee4c8a73c53c239d40e78e9749
wsws1996 <1786578565@qq.com>
2016-01-19 20:52:41 +0800

    第十七天完成

---
+++ 第十七天/图片验证码/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 图片验证码
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            string str = null;
+            for (int i = 0; i < 5; i++)
+            {
+                int rNumber = r.Next(0, 10);
+                str += rNumber;
+            }
+            Bitmap bmp = new Bitmap(120, 40);
+            Graphics g = Graphics.FromImage(bmp);
+            for (int i = 0; i < 5; i++)
+            {
+                Point p = new Point(i * 20, 0);
+                string[] fonts = { "微软雅黑", "宋体", "黑体", "隶书", "仿宋" };
+                Color[] colors = { Color.Pink, Color.Blue, Color.Black, Color.Red, Color.Green };
+                g.DrawString(str[i].ToString(), new Font(fonts[r.Next(0, 5)], 30, FontStyle.Italic), new SolidBrush(colors[r.Next(0, 5)]), p);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Point p1 = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+                Point p2 = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+                g.DrawLine(new Pen(Brushes.Green), p1, p2);
+            }
+
+            for (int i = 0; i < 500; i++)
+            {
+                Point p = new Point(r.Next(0, bmp.Width), r.Next(0, bmp.Height));
+                bmp.SetPixel(p.X, p.Y, Color.Black);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
